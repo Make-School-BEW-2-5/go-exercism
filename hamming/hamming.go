96b5aaa4908f0f70513cdca9b688cc4e682d46a1
Severiano Badajoz <sevebadajoz@gmail.com>
2019-01-24 14:40:28 -0800

    Submit challenge: hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,19 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
+
+	var distance int
+
+	if len(a) != len(b) {
+		return -1, errors.New("Inequal sequence lengeths")
+	}
+	for i, nucelotide := range a {
+		if nucelotide != rune(b[i]) {
+			distance += 1
+		}
+	}
+
+	return distance, nil
 }
