ca89eff888d656d17edeed176737692fb0c04d35
Severiano Badajoz <sevebadajoz@gmail.com>
2019-02-07 14:14:14 -0800

    Begin exercise, create numbers array

---
+++ twelve-days/twelve_days.go
@@ -0,0 +1,20 @@
+package twelve
+
+var numbers = [][]string{
+	{"one","a Partridge in a Pear Tree"},
+	{"two","two Turtle Doves"},
+	{"three","three French Hens"},
+	{"four","four Calling Birds"},
+	{"five","five Gold Rings"},
+	{"six","six Geese-a-Laying"},
+	{"seven","seven Swans-a-Swimming"},
+	{"eight","eight Maids-a-Milking"},
+	{"nine","nine Ladies Dancing"},
+	{"ten","ten Lords-a-Leaping"},
+	{"eleven","eleven Pipers Piping"},
+	{"twelve","twelve Drummers Drumming"}
+}
+
+func Verse(line int) string {
+	
+}
